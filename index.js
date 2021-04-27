2f5cd81c911bcc1456192162844d080103c3fb4b
Rajhimanshu203 <rajhimanshu203@gmail.com>
2021-04-27 18:41:35 +0530

    go

---
+++ index.js
@@ -0,0 +1,12 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+app.listen(3000 , function()
+{
+    console.log("Server started on port 3000")
+});
+
+app.get("/" , function(request,response){
+console.log(request)
+response.sendFile(__dirname + "/index.html");
+});
